0f1bee731514e279b42a5d155ada1f31a1f3e202
berton7 <francy.berton99@gmail.com>
2021-01-24 14:53:38 +0000

    Use pretty print

--- utils/list_db.py
+++ utils/list_db.py
@@ -1,5 +1,6 @@
 from utils.shoe_manager import ShoeManager
 from utils.tools import get_logger
+from pprint import pprint
 
 if __name__ == "__main__":
 	l = get_logger("Lister")
@@ -7,6 +8,6 @@ if __name__ == "__main__":
 
 	if s.db_name in s._client.list_database_names():
 		for item in s._db.find({}, {"_id": 0}):
-			print(item)
+			pprint(item)
 	else:
 		print("Database does not exist")
