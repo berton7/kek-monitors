8e66d30e5e8070fb3cd4d9bc365b59d984568e78
berton7 <francy.berton99@gmail.com>
2021-01-30 14:11:43 +0100

    Updated footdistrict

--- monitors/footdistrict.py
+++ monitors/footdistrict.py
@@ -1,12 +1,16 @@
 import asyncio
-from utils.tools import make_default_executable
+import json
+from typing import List
 
 from bs4 import BeautifulSoup
 from fake_headers import Headers
+from pyppeteer.launcher import launch
+from pyppeteer.network_manager import Response
+from pyppeteer.page import Page
 from utils.shoe_stuff import Shoe
+from utils.tools import make_default_executable
 
 from monitors.base_monitor import BaseMonitor
-import json
 
 
 class Footdistrict(BaseMonitor):
@@ -14,30 +18,60 @@ class Footdistrict(BaseMonitor):
 		# create a random headers generator, configured to generate random windows headers
 		self.headers_gen = Headers(os="win", headers=True)
 
+	async def async_init(self):
+		self.browser = await launch()
+		self.context = await self.browser.createIncognitoBrowserContext()
+		await self.context.newPage()
+
+	async def on_async_shutdown(self):
+		await self.browser.close()
+
+	async def get_fd_page(self, link: str, page: Page):
+		await page.setExtraHTTPHeaders(self.headers_gen.generate())
+		await page.setJavaScriptEnabled(True)
+		self.network_logger.debug(f"{link}: getting...")
+		await page.goto(link)
+		self.network_logger.debug(f"{link}: waiting to redirect and stuff...")
+		response = await page.waitForResponse(lambda res: res.url == link and res.status == 200, {"timeout": 10000})
+		if response.ok:
+			# await page.waitForNavigation()
+			self.network_logger.debug(f"{link}: has loaded")
+			await page.setJavaScriptEnabled(False)
+			await page.keyboard.press("Escape")
+		else:
+			self.network_logger.warning(f"{link}: failed to get: {response.status}")
+		return response
+
 	async def loop(self):
 		# current links are contained in self.links
 		if not self.links:
 			self.links = [
-				"https://footdistrict.com/en/adidas-rivalry-hi-x-star-wars-chewbacca-fx9290.html"]
+				"https://footdistrict.com/nike-air-max-95-ndstrkt-cz3591-001.html"]
 		self.general_logger.debug(f"Links are: {self.links}")
 
 		# tasks will contain asynchronous tasks to be executed at once asynchronously
 		# in this case, they will contain the requests to the links
 		tasks = []
+		pages = []  # type: List[Page]
 		for link in self.links:
-			tasks.append(self.fetch(link, headers=self.headers_gen.generate()))
+			page = await self.context.newPage()
+			pages.append(page)
+			tasks.append(self.get_fd_page(link, page))
 
 		# gather, execute all tasks
-		r = await asyncio.gather(*tasks)
+		responses = await asyncio.gather(*tasks)  # type: List[Response]
 
-		for link, pack in zip(self.links, r):
-			response, text = pack
-			if not response:
-				# error is already reported by NetworkUtils
-				continue
-			if response.code >= 400:
+		for link, page, response in zip(self.links, pages, responses):
+			if not response.ok:
 				self.general_logger.debug(
-					f"{response.effective_url}: skipping parsing on code {response.code}")
+					f"{link}: skipping parsing on code {response.code}")
+				continue
+
+			text = await page.content()
+			await page.close()
+			if len(text) < 1000:
+				self.general_logger.warning(
+					f"Failed to get {link} (len of response: {len(text)})")
 				continue
 
 			# BeautifulSoup can be used to parse html pages in a very convenient way
@@ -46,26 +80,21 @@ class Footdistrict(BaseMonitor):
 			# create a Shoe object to hold information
 			s = Shoe()
 			# parse all the page. for simplicity here we only get the name
-			s.name = soup.find("div", {"id": "product_text"}).h1.get_text()
+			s.name = soup.find("meta", {"property": "og:title"}).get("content")
 			s.link = link
-			s.img_link = soup.find("a", {"id": "zoom1"}).find(
-				"img", {"cloudzoom"}).get("src")
-			for script in soup.find_all("script", {"type": "text/javascript"}):
-				if script.string:
-					if script.string.find("var spConfig") != -1:
-						firststr = ".Config("
-						script_t = script.string
-						fi = script_t.find(firststr) + len(firststr)
-						li = script_t.find(");")
-						spConfig = script_t[fi:li]
-						spConfig = json.loads(spConfig)
-						for size in spConfig["attributes"]["134"]["options"]:
-							sizename = size["label"]
-							index = sizename.find(" * Not available")
-							available = index == -1
-							if not available:
-								sizename = sizename[:index]
-							s.sizes[sizename] = {"available": available}
+			# https://footdistrict.com/media/resize/500x333/catalog/product/p/r/producto_02_10_2063_4/adidas-rivalry-hi-x-star-wars-chewbacca-fx9290-0.webp
+			# s.img_link = soup.find("div", {"id": "productos-ficha-item"}).find(
+			#	"img", {"class": "img-responsive lazyloaded"}).get("src")
+			for script in soup.find_all("script", {"type": "text/x-magento-init"}):
+				script_text = script.string
+				if script_text.find("jsonConfig") != -1:
+					script_json = json.loads(script_text)
+					options = script_json["[data-role=swatch-options]"]["Magento_Swatches/js/swatch-renderer"]["jsonConfig"]["attributes"]["134"]["options"]
+					for opt in options:
+						sizename = opt["label"]
+						available = bool(opt["products"])
+						s.sizes[sizename] = {"available": available}
+					break
 			else:
 				pass
 
--- scrapers/footdistrict.py
+++ scrapers/footdistrict.py
@@ -1,8 +1,12 @@
 import asyncio
-from utils.tools import make_default_executable
+from typing import List
 
 from bs4 import BeautifulSoup
 from fake_headers import Headers
+from pyppeteer import launch
+from pyppeteer.network_manager import Response
+from pyppeteer.page import Page
+from utils.tools import make_default_executable
 
 from scrapers.base_scraper import BaseScraper
 
@@ -11,7 +15,7 @@ class Footdistrict(BaseScraper):
 	def init(self):
 		# website infos
 		self.base_url = "https://footdistrict.com"
-		self.endpoints = ["/en/sneakers.html"]
+		self.endpoints = ["/zapatillas/f/b/nike/"]
 
 		# create a random headers generator, configured to generate random windows headers
 		self.headers_gen = Headers(os="win", headers=True)
@@ -19,42 +23,73 @@ class Footdistrict(BaseScraper):
 		# max links to be monitored
 		self.max_links = 5
 
+	async def async_init(self):
+		self.browser = await launch()
+		self.context = await self.browser.createIncognitoBrowserContext()
+		await self.context.newPage()
+
+	async def on_async_shutdown(self):
+		await self.browser.close()
+
+	async def get_fd_page(self, link: str, page: Page):
+		await page.setExtraHTTPHeaders(self.headers_gen.generate())
+		await page.setJavaScriptEnabled(True)
+		self.network_logger.debug(f"{link}: getting...")
+		r = await page.goto(link)
+		self.network_logger.debug(f"{link}: waiting to redirect and stuff...")
+		response = await page.waitForResponse(lambda res: res.url == link and res.status <= 300, {"timeout": 10000})
+		if response.ok:
+			# await page.waitForNavigation()
+			await asyncio.sleep(1.5)  # give the page some time to load
+			self.network_logger.debug(f"{link}: has loaded")
+			await page.setJavaScriptEnabled(False)
+			await page.keyboard.press("Escape")
+		else:
+			self.network_logger.warning(f"{link}: failed to get: {response.status}")
+		return response
+
 	async def loop(self):
 		# tasks will contain asynchronous tasks to be executed at once, asynchronously
 		# in this case, they will contain the requests to the endpoints
+		pages = []  # type: List[Page]
 		tasks = []  # List[Coroutine]
 		# create a task for each endpoint
 		for ep in self.endpoints:
-			tasks.append(self.fetch(self.base_url +
-                           ep, headers=self.headers_gen.generate()))
+			page = await self.context.newPage()
+			pages.append(page)
+			tasks.append(self.get_fd_page(self.base_url + ep, page))
 
 		# gather, execute all tasks
 		responses = await asyncio.gather(*tasks)
 
-		for response, text in responses:
-			if not response:
-				# error is already reported by NetworkUtils
+		for link, page, response in zip(self.endpoints, pages, responses):
+			if not response.ok:
+				self.general_logger.debug(
+					f"{link}: skipping parsing on code {response.code}")
 				continue
 
+			text = await page.content()
 			# BeautifulSoup can be used to parse html pages in a very convenient way
 			soup = BeautifulSoup(text, "lxml")
 
 			# parsing example. in this case we simply add the first self.max_links products.
-			grid = soup.find("div", {"class": "category-products"})
+			grid = soup.find("ol", {"class": "product-items"})
 			count = 0
-			for row in grid.find_all("ul", {"class": "products-grid"}):
-				for prod in row.find_all("li"):
-					count += 1
-					if count <= self.max_links:
-						link = prod.a.get("href")
-						if link not in self.links:
-							self.links.append(link)
-							self.general_logger.info(f"Found {link}")
-							# if you want to send every link immediately (not recommended):
-							# self.links = [link]
-							# await self.add_links()
-				break
+			for prod in grid.find_all("li"):
+				count += 1
+				if count <= self.max_links:
+					link = prod.a.get("href")
+					if link not in self.links:
+						self.links.append(link)
+						self.general_logger.info(f"Found {link}")
+						# if you want to send every link immediately (not recommended):
+						# self.links = [link]
+						# await self.add_links()
+				else:
+					break
 
+			await page.close()
+		pass
 		# at the end of each self.loop(), self.update_links() is called to send the links to the monitor, if they have changed since the previous loop
 
 
