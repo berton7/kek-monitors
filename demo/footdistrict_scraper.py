6a9b90a1377efc70773e0eb76e857f650b5c5923
berton7 <francy.berton99@gmail.com>
2021-02-08 15:52:20 +0100

    Removed debug lines

--- demo/footdistrict_scraper.py
+++ demo/footdistrict_scraper.py
@@ -1,9 +1,3 @@
-if __name__ == "__main__":
-	import sys
-	import os
-	sys.path.insert(0, os.path.abspath(
-		os.path.join(os.path.dirname(__file__), '..')))
-
 import asyncio
 from typing import List
 
